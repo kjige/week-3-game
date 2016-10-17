44726cfdc8d817cc33c70b7f1ad5c306b29828eb
kjige <chrisleongsc2@gmail.com>
2016-10-17 18:15:43 -0500

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var lettersGuessed = [];
+var guessesRemaining = 10;
+var wins = 0;
+var words = ['cheeseburger', 'tsunami', 'volleyball', 'sunburn', 'bikini', 'frisbee'];
+var currentWord = ['honey'];
+
+console.log(currentWord[0][1]);
+
+var test = words[math.floor(math.random()*words.length)];
+
+console.log('test');
