3123d542f55b1f2098c291407930e8a6b2985fe6
kjige <chrisleongsc2@gmail.com>
2016-10-19 14:34:51 -0500

    working game without reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,10 @@ var guessesRemaining = 10;
 var wins = 0;
 var words = ['cheeseburger', 'tsunami', 'volleyball', 'sunburn', 'bikini', 'frisbee', 'swimming', 'umbrella', 'ocean', 'sharks', 'surfing', 'paddleboard', 'scuba', 'jellyfish', 'sunset'];
 var currentWord = '';
+var workingWord = [];
 var lettersGuessed = [' '];
+var letter;
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 var lettersGuessedId = document.getElementById('lettersGuessed');
 var currentWordId = document.getElementById('currentWord');
 
@@ -16,19 +19,20 @@ console.log(currentWord);
 //Creates blanks for current word
 for (var i = 0; i < currentWord.length; i++) {
 	var newBlank = document.createElement('li');
-	newBlank.setAttribute('class', 'display-inline');
-	newBlank.innerHTML = '_ ';
+	newBlank.setAttribute('class', 'display-inline padding');
+	newBlank.setAttribute('id', 'blank');
+	newBlank.innerHTML = '_';
 	currentWordId.appendChild(newBlank);
+	workingWord.push('_');
 }
 
-// When player presses key, check in alphabet array for match
-var letter;
-var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+// Store pressed 
 document.onkeyup = function(event) {
 	letter = String.fromCharCode(event.keyCode).toLowerCase(); 
 	checkAlphabet();
 }
 
+// Check alphabet array
 function checkAlphabet() {
 	for (var i = 0; i < alphabet.length; i++) {
 		if (letter === alphabet[i]) {
@@ -47,7 +51,25 @@ function checkLettersGuessed() {
 }
 
 function checkCurrentWord() {
-	
+	for (var i = 0; i < currentWord.length; i++) {
+		if (letter === currentWord[i]) {
+			workingWord[i] = letter;
+			replaceLetter();
+		}
+	}
+}
+
+function replaceLetter() {
+	currentWordId.innerHTML = workingWord[i];
+	var removeBlanks = document.getElementById('blank');
+	currentWordId.removeChild(currentWordId.firstChild);
+	for (var i = 0; i < workingWord.length; i++) {
+		var newBlankOrLetter = document.createElement('li');
+		newBlankOrLetter.setAttribute('class', 'display-inline padding');
+		newBlankOrLetter.setAttribute('id', 'blank');
+		newBlankOrLetter.innerHTML = workingWord[i];
+		currentWordId.appendChild(newBlankOrLetter);
+	}
 }
 
 function useLetter() {
@@ -58,7 +80,5 @@ function useLetter() {
 	newLetter.setAttribute('class', 'display-inline padding');
 	lettersGuessedId.appendChild(newLetter);
 }
-
-
 
 }
