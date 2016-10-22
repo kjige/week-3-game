622c19a7fd96516ab935725be7effa33d5e7a23e
kjige <chrisleongsc2@gmail.com>
2016-10-21 23:24:46 -0500

    blanks error

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,30 @@
 // Variables
-var guessesRemaining = 10;
-var wins = 0;
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var words = ['cheeseburger', 'tsunami', 'volleyball', 'sunburn', 'bikini', 'frisbee', 'swimming', 'umbrella', 'ocean', 'sharks', 'surfing', 'paddleboard', 'scuba', 'jellyfish', 'sunset'];
 var currentWord = [];
 var workingWord = [];
 var lettersGuessed = [];
+var guessesRemaining = 10;
+var wins = 0;
 var letter;
+// var snd = new Audio("assets/images/boing2.wav");
 var winsId = document.getElementById('wins');
-var winCounterId = document.getElementById('winCounter');
 var currentWordId = document.getElementById('currentWord');
-var blanksId = document.getElementById('blank');
 var remainingId = document.getElementById('remaining');
-var livesId = document.getElementById('lives');
 var lettersGuessedId = document.getElementById('lettersGuessed');
 var gameStatusId = document.getElementById('gameStatus');
-var playId = document.getElementById('play');
-var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-// Selects new word and splits each character into an array
-var selectWord = words[Math.floor(Math.random()*words.length)];
-currentWord = selectWord.split("");
-console.log(currentWord);
-
-//Creates blanks for current word
 function play() {
+	// for (var i = 0; i < workingWord.length; i++) {
+	// 	var blanksId = document.getElementById('blank');
+	// 	blanksId.parentNode.removeChild(blanksId);
+	// }
+	// Selects new word and splits each character into an array
+	var selectWord = words[Math.floor(Math.random()*words.length)];
+	currentWord = selectWord.split("");
+	console.log(currentWord);
+	// Creates blanks for current word
 	for (var i = 0; i < currentWord.length; i++) {
-		currentWordId.removeChild(blanksId);
 		var newBlank = document.createElement('li');
 		newBlank.setAttribute('class', 'display-inline padding');
 		newBlank.setAttribute('id', 'blank');
@@ -34,6 +33,7 @@ function play() {
 		workingWord.push('_');
 	}
 }
+
 // Start game
 play();
 
@@ -51,7 +51,7 @@ document.onkeyup = function(event) {
 						updateRemaining();
 						play();
 					} 
-				 } else if (checkCurrentWord() === false) {
+				} else {
 					subtractRemaining();
 					updateRemaining();
 					updateUsedLetters();
@@ -111,13 +111,17 @@ function checkCurrentWord() {
 // re-writes display of current word with letters and blanks
 function replaceLetter() {
 	for (var i = 0; i < workingWord.length; i++) {
-		currentWordId.removeChild(blanksId);
+		var blanksId = document.getElementById('blank');
+		blanksId.parentNode.removeChild(blanksId);
+		var currentWordId = document.getElementById('currentWord');
 		var newBlankOrLetter = document.createElement('li');
 		newBlankOrLetter.setAttribute('class', 'display-inline padding');
 		newBlankOrLetter.setAttribute('id', 'blank');
 		newBlankOrLetter.innerHTML = workingWord[i];
 		currentWordId.appendChild(newBlankOrLetter);
+		// snd.play();
 	}
+	console.log(workingWord);
 }
 
 // subtracts 1 from remaining number of guesses
@@ -128,7 +132,8 @@ function subtractRemaining() {
 
 // updates remaining guesses left in DOM
 function updateRemaining() {
-	remainingId.removeChild(livesId);
+	var livesId = document.getElementById('lives');
+	livesId.parentNode.removeChild(livesId);
 	var newScore = document.createElement('li');
 	newScore.setAttribute('id', 'lives');
 	newScore.setAttribute('class', 'display-inline padding')
@@ -152,10 +157,12 @@ function winStatus() {
 			isComplete = false;
 		}
 	}
-	if (isComplete) {
-		gameStatusId.removeChild(playId);
+	if (isComplete === true) {
+		var playId = document.getElementById('play');
+		playId.parentNode.removeChild(playId);
 		var newgameStatus = document.createElement('li');
 		newgameStatus.setAttribute('class', 'display-inline');
+		newgameStatus.setAttribute('id', 'play');
 		newgameStatus.innerHTML = 'You Win!';
 		gameStatusId.appendChild(newgameStatus);
 		wins += 1;
@@ -166,9 +173,11 @@ function winStatus() {
 
 // updates win counter
 function winCounter() {
-	winsId.removeChild(winCounterId);
+	var winCounterId = document.getElementById('winCounter');
+	winCounterId.parentNode.removeChild(winCounterId);
 	var newWinCounter = document.createElement('li');
 	newWinCounter.setAttribute('class', 'display-inline');
+	newWinCounter.setAttribute('id', 'winCounter');
 	newWinCounter.innerHTML = wins;
 	winsId.appendChild(newWinCounter);
 }
@@ -176,8 +185,11 @@ function winCounter() {
 // updates game status to game over
 function lose() {
 	if (guessesRemaining < 1) {
-		gameStatusId.removeChild(playId);
+		var playId = document.getElementById('play');
+		playId.parentNode.removeChild(playId);
 		var newgameStatus = document.createElement('li');
+		newgameStatus.setAttribute('id', 'play');
+		newgameStatus.setAttribute('class', 'display-inline');
 		newgameStatus.innerHTML = 'Game Over';
 		gameStatusId.appendChild(newgameStatus);
 		return true;
@@ -186,13 +198,14 @@ function lose() {
 }
 
 function resetGame() {
-	document.onkeyup = function(){
 		guessesRemaining = 10;
 		currentWord = [];
 		workingWord = [];
 		lettersGuessed = [];
-		play();
-	}
+		for (var i = 0; i < currentWord.length; i++) {	
+			var blanksId = document.getElementById('blank');
+			blanksId.parentNode.removeChild(blanksId);
+		}
 }
 
 
